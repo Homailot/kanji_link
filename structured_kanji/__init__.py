d158dfde38f003dc73cfa7a5191b190c1054e7cb
Nuno Alves <nyansword@gmail.com>
2024-12-29 00:43:37 +0000

    Vendor pycapnp win

--- structured_kanji/__init__.py
+++ structured_kanji/__init__.py
@@ -1,12 +1,16 @@
 from aqt import mw
 from aqt.qt import *
+from pathlib import Path
+
+ADDON_DIR = Path(__file__).resolve().parent
+sys.path.append(str(ADDON_DIR / "vendor"))
 
 from .migrate.gui.deckselect import MigrateDialog
-
 
 def open_migrate() -> None:
     mw.dialog = MigrateDialog(mw.app.activeWindow())
     mw.dialog.exec()
+
 
 
 action = QAction("Migrate Deck to Structured Kanji", mw)
